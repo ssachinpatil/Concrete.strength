5a8f5063a374d79cf54f36dd83fd6918d7ff32f1
ssachinpatil <115664776+ssachinpatil@users.noreply.github.com>
2022-10-24 01:10:53 -0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
+import json
 
 PORT_NUMBER=3004
-JSON_FILE_PATH=os.path.join('project_app','data.json')
+JSON_FILE_PATH=os.path.join(os.getcwd(),r'data.json')
 
-MODEL_FILE_PATH=os.path.join('project_app','concrete.pkl')
+MODEL_FILE_PATH=os.path.join(os.getcwd(),'concrete.pkl')
